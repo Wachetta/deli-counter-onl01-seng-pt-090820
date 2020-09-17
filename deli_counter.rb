2a52d624bf93b9b6c776e98ab793be1676182f4c
Zachary Darneille <zakdarn@gmail.com>
2020-09-17 22:23:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+
+>>>>>>> b82d9b1778efd19a4042c33de0f0ac9ac8033fa3
 def line(array)
 start = "The line is currently:"
     if array  == []
@@ -12,6 +16,7 @@ start = "The line is currently:"
 end
 
 def take_a_number(deli, person)
+<<<<<<< HEAD
   if deli.length == 0
      deli << person
     puts "Welcome, #{person}. You are number 1 in line."
@@ -29,4 +34,21 @@ def now_serving(deli)
     puts "Currently serving #{deli[0]}."
 deli.shift
   end
+=======
+  line = []
+  if person >= 1 #if no people are in line it add person
+    person.each_with_index do |x, idx|
+      line.unshift "Welcome #{x}. You are number #{idx+1} in line."
+else
+  #if some people are in line it puts you to back of line
+      person.each_with_index do |y, idy|
+          line << "Welcome #{y}. You are number #{idy+1}in line"
+      end
+    end
+end
+end
+
+def now_serving
+
+>>>>>>> b82d9b1778efd19a4042c33de0f0ac9ac8033fa3
 end
